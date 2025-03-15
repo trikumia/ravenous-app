666b7629d4405a0b02b02443010cf488ade9a9d9
Panos cinefog <geroulakou.k@gmail.com>
2025-03-15 15:28:41 +0200

    add files to directories

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import './Business.css';
+import Business from './Business';
+
+const BusinessList = () => {
+    return (
+        <div className="list">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    );
+};
+
+export default BusinessList;
